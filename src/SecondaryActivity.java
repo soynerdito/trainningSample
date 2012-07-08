d0062f484fbfb76610d57b6611a03c730913c553
Soynerdito <soynerdito@gmail.com>
2012-07-08 12:36:57 -0400

    small change

--- src/SecondaryActivity.java
+++ src/SecondaryActivity.java
@@ -1,6 +1,13 @@
 import android.app.Activity;
+import android.os.Bundle;
 
 
 public class SecondaryActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
 
 }
