a6ad51cf4cc452b65bff2ea35659790e5b76d418
Soynerdito <soynerdito@gmail.com>
2012-07-08 11:36:26 -0400

    Test Commit, starting to add files to project

---
+++ src/com/example/widgets/ToolBar.java
@@ -0,0 +1,19 @@
+package com.example.widgets;
+
+import com.example.sampleapp.R;
+import android.app.Service;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.RelativeLayout;
+
+public class ToolBar extends RelativeLayout {
+
+	public ToolBar(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub		
+		LayoutInflater li = (LayoutInflater) context.getSystemService(Service.LAYOUT_INFLATER_SERVICE);
+		li.inflate(R.layout.toolbar, null);
+	}
+
+}
